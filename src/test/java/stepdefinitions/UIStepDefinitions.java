a4474f6302a5a305a9eac3f9b13adbf7724f1d4e
Feyza Ocakdan <ocakdanfeyza@gmail.com>
2023-01-12 20:47:27 +0300

    update

--- src/test/java/stepdefinitions/UIStepDefinitions.java
+++ src/test/java/stepdefinitions/UIStepDefinitions.java
@@ -22,14 +22,11 @@ public class UIStepDefinitions {
     public void user_is_on_home_page() {
         Driver.getDriver().get(ConfigReader.getProperty("base_url"));
     }
-
     @When("user click on UyePanel i button")
     public void userClickOnUyePanelIButton() {
-        //actions.moveToElement(homePage.yeni_uyelik);
         homePage.uye_panel.click();
 
     }
-
     @And("user click on Giris")
     public void userClickOnGiris() {
         homePage.giris.click();
@@ -37,81 +34,97 @@ public class UIStepDefinitions {
 
     @Then("user verifies that it seems UYE GIRIS EKRANI header")
     public void userVerifiesThatItSeemsUYEGIRISEKRANIHeader() {
-        Assert.assertEquals("ÜYE GİRİŞ EKRANI",homePage.header.getText());
+        Assert.assertEquals("ÜYE GİRİŞ EKRANI", homePage.header.getText());
     }
-
-    @And("user enters email textbox")
-    public void userEntersEmailTextbox() {
-        homePage.first_box.sendKeys("");
-    }
-
-    @And("user enters password the box")
-    public void userEntersPasswordTheBox() {
-        homePage.second_box.sendKeys("");
-    }
-
     @And("user click on Yeni Uyelik")
     public void userClickOnYeniUyelik() throws InterruptedException {
-        Thread.sleep(3000);
+        Thread.sleep(4000);
         homePage.yeni_uyelik.click();
     }
-
     @Then("user verifies that on the uyelik page")
     public void userVerifiesThatOnTheUyelikPage() {
         Assert.assertTrue(Driver.getDriver().getCurrentUrl().contains("uyelik"));
     }
-
     @And("user select any country")
     public void userSelectAnyCountry() {
-        Select select =new Select(homePage.ulke_box);
+        Select select = new Select(homePage.ulke_box);
         select.selectByIndex(1);
     }
-
     @And("user select any city")
     public void userSelectAnyCity() {
-        Select select =new Select(homePage.sehir_box);
+        Select select = new Select(homePage.sehir_box);
         select.selectByIndex(6);
     }
-
     @And("user select official institution")
     public void userSelectOfficialInstitution() {
-        Select select =new Select(homePage.uyelik_tipi_box);
+        Select select = new Select(homePage.uyelik_tipi_box);
         select.selectByIndex(1);
     }
-
     @And("user select producer")
     public void userSelectProducer() throws InterruptedException {
-        Select select =new Select(homePage.uyelik_turu_box);
+        Select select = new Select(homePage.uyelik_turu_box);
         Thread.sleep(1000);
         select.selectByIndex(1);
     }
-
-    @When("user enters {string},{string},{string},{string},{string},{string},{string},{string},{string},{string},{string},{string}")
-    public void userEnters(String company_name, String company_iban, String company_website, String company_email, String company_adress, String tax_box, String tax_number, String land_phone, String mobile_phone, String official_name_surname, String official_email, String official_phone) {
+    @When("user enters sirket_unvani {string}")
+    public void userEntersSirket_unvani(String company_name) {
         homePage.company_name.sendKeys(company_name);
+    }
+    @When("user enters kurumsal_iban {string}")
+    public void userEntersKurumsal_iban(String company_iban) {
         homePage.company_iban.sendKeys(company_iban);
+    }
+    @When("user enters kurumsal_web_sitesi{string}")
+    public void userEntersKurumsal_web_sitesi(String company_website) {
         homePage.company_website.sendKeys(company_website);
+    }
+    @When("user enters kurumsal_e_posta {string}")
+    public void userEntersKurumsal_e_posta(String company_email) {
         homePage.company_email.sendKeys(company_email);
+    }
+    @When("user enters sirket_adresi {string}")
+    public void userEntersSirket_adresi(String company_adress) {
         homePage.company_adress.sendKeys(company_adress);
+    }
+    @When("user enters vergi_dairesi {string}")
+    public void userEntersVergi_dairesi(String tax_box) {
         homePage.tax_box.sendKeys(tax_box);
-        homePage.tax_number.sendKeys( tax_number);
+    }
+    @When("user enters vergi_numarasi {string}")
+    public void userEntersVergi_numarasi(String tax_number) {
+        homePage.tax_number.sendKeys(tax_number);
+    }
+    @When("user enters sabit_telefon {string}")
+    public void userEntersSabit_telefon(String land_phone) {
         homePage.land_phone.sendKeys(land_phone);
-        homePage.mobile_phone.sendKeys( mobile_phone);
+    }
+    @When("user enters mobil_telefon {string}")
+    public void userEntersMobil_telefon(String mobile_phone) {
+        homePage.mobile_phone.sendKeys(mobile_phone);
+    }
+    @When("user enters yetkili_ad_soyad {string}")
+    public void userEntersYetkili_ad_soyad(String official_name_surname) {
         homePage.official_name_surname.sendKeys(official_name_surname);
+    }
+    @When("user enters yetkili_e_posta {string}")
+    public void userEntersYetkili_e_posta(String official_email) {
         homePage.official_email.sendKeys(official_email);
+    }
+    @When("user enters yetkili_gsm {string}")
+    public void userEntersYetkili_gsm(String official_phone) {
         homePage.official_phone.sendKeys(official_phone);
+
     }
     @And("user click on agreement")
-    public void userClickOnAgreement() {
-
+    public void userClickOnAgreement() throws InterruptedException {
+        Thread.sleep(2000);
         homePage.agreement.click();
-
     }
     @And("user click on enrol button")
-    public void userClickOnEnrolButton() {
+    public void userClickOnEnrolButton() throws InterruptedException {
+        Thread.sleep(1000);
         homePage.enrol.click();
     }
-
     @And("user close browser")
     public void userCloseBrowser() {
         Driver.closeDriver();
--- target/test-classes/features/firstfeaturefile.feature
+++ target/test-classes/features/firstfeaturefile.feature
@@ -1,22 +1,36 @@
 @Test01
 Feature: TestPage
+
   Background:
     Given user is on homePage
     When user click on UyePanel i button
     And user click on Yeni Uyelik
     Then user verifies that on the uyelik page
+
   @Test02
   Scenario Outline: Test_the_page
     Given user select official institution
     And user select any country
     And user select producer
     And user select any city
-    When user enters "<company_name>","<company_iban>","<company_website>","<company_email>","<company_adress>","<tax_box>","<tax_number>","<land_phone>","<mobile_phone>","<official_name_surname>","<official_email>","<official_phone>"
-  # And user click on agreement
+    When user enters sirket_unvani "<company_name>"
+    When user enters kurumsal_iban "<company_iban>"
+    When user enters kurumsal_web_sitesi"<company_website>"
+    When user enters kurumsal_e_posta "<company_email>"
+    When user enters sirket_adresi "<company_adress>"
+    When user enters vergi_dairesi "<tax_box>"
+    When user enters vergi_numarasi "<tax_number>"
+    When user enters sabit_telefon "<land_phone>"
+    When user enters mobil_telefon "<mobile_phone>"
+    When user enters yetkili_ad_soyad "<official_name_surname>"
+    When user enters yetkili_e_posta "<official_email>"
+    When user enters yetkili_gsm "<official_phone>"
+    And user click on agreement
     And user click on enrol button
+     And user close browser
     Examples:
-      | company_name                 |company_iban             | company_website | company_email     | company_adress                    | tax_box | tax_number | land_phone     | mobile_phone   | official_name_surname | official_email   | official_phone |
-      | AbgroupAbgroupAbgroupAbgroup | TR1234000000200012233412 | www.abgroup.com | abgroup@gmail.com | kartal mah. cevizli sok numara 18 | Kartal  | 1234567890 | 5071246567 |5346768768 | Musa Yıldız           | abberk123@gmail.com |5379789789|
+      | company_name                 | company_iban             | company_website | company_email     | company_adress                    | tax_box | tax_number | land_phone   | mobile_phone | official_name_surname | official_email      | official_phone |
+      | AbgroupAbgroupAbgroupAbgroup | TR1234000000200012233412 | www.abgroup.com | abgroup@gmail.com | kartal mah. cevizli sok numara 18 | Kartal  | 1234567890 | 507-126-4567 | 5346768768   | Musa Yıldız           | abberk123@gmail.com | 5379789789     |
 
 
 
